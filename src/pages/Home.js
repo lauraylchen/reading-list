f1438c48f85d967b6026060fe26b92fe75dd1f9c
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-08-09 11:18:34 -0400

    Custom hook for fetching real-time data from firestore useCollection and apply to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,12 @@
-import { useState, useEffect } from 'react'
-
 // components
 import BookList from '../components/BookList'
 import BookForm from '../components/BookForm'
 
-// data
-import { db } from '../firebase/config'
-import { collection, getDocs } from "firebase/firestore";
+// hooks
+import { useCollection } from '../hooks/useCollection'
 
 export default function Home() {
-  const [books, setBooks] = useState(null)
-
-  useEffect(() => {
-    const ref = collection(db, 'books')
-
-    getDocs(ref)
-      .then((snapshot) => {
-        let results = []
-        snapshot.docs.forEach( doc => {
-          results.push({id: doc.id, ...doc.data()})
-        })
-        setBooks(results)
-      })
-  }, [])
+  const { documents: books } = useCollection('books')
 
   return (
     <div>
