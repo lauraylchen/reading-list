5d6a9fcdde394f94228f9f8fc3fd6a34986205d3
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-08-09 10:47:41 -0400

    Set up firebase and firestore service

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFsnhdgM5qwNHwdc8ZYsUIEdosXaEr0mU",
+  authDomain: "readinglist-site.firebaseapp.com",
+  projectId: "readinglist-site",
+  storageBucket: "readinglist-site.appspot.com",
+  messagingSenderId: "1038778644758",
+  appId: "1:1038778644758:web:5d7d338983598e65a6b837"
+};
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init firestore
+const db = getFirestore()
+
+export { db }
